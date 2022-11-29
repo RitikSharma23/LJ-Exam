990ab477ad648adf32cc5939c677824fe3843f8d
RitikSharma23 <100440065+RitikSharma23@users.noreply.github.com>
2022-11-29 19:17:52 +0530

    commit

--- exam_management/exam_management/urls.py
+++ exam_management/exam_management/urls.py
@@ -1,4 +1,4 @@
-"""exam_management URL Configuration
+"""myweb URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+    path("", include("home.urls")),
+    path("adminpage", include("adminpage.urls")),
+    # path("login/", include("login.urls")),
+    # path("register/", include("register.urls")),
     path("admin/", admin.site.urls),
 ]
