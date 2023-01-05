eac206f64cae5e70cbbfea69923cb97368a7dd82
RitikSharma23 <100440065+RitikSharma23@users.noreply.github.com>
2023-01-05 20:25:00 +0530

    commit

--- exam_management/result/views.py
+++ exam_management/result/views.py
@@ -24,24 +24,67 @@ def viewresult(request):
 def find(request):
     data=request.POST
     d=Subject.objects.filter(sem=1,institute_code='004',program_code='002').values()
-
-    query=""
-    theory=list()
-    practical=list()
-    mid=list()
-
+    grade=Grade.objects.values().all()
+    col=list()
+    for a in d:
+        r=""
+        if(a['theory']):r=r+",1_"+(str(a['subjectcode'])+"_t")
+        if(a['practical']):r=r+",1_"+(str(a['subjectcode'])+"_p")
+        if(a['mid']):r=r+",1_"+(str(a['subjectcode'])+"_m")
+        col.append(r)
 
     mydb=mysql.connector.connect(**config)
     mycursor = mydb.cursor()    
-    subject=dict()
 
-    co=("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'adminpage_studentmarks' AND COLUMN_NAME LIKE '1_%';")
-    mycursor.execute(co)
-    column = mycursor.fetchall()
+    res=list()
+    for  q in col:
+        co=("SELECT enrollment"+q+" FROM `adminpage_studentmarks` WHERE enrollment='21004500210001'")
+        mycursor.execute(co)
+        res.append(mycursor.fetchall())
+    x=0
 
-    for c in column:
-        print(c[0])
+    marks=list()
     
+    for i in res:
+        temp=list()
+        t = ast.literal_eval(i[0][1])
+
+        temp.append(d[x]['subjectcode'])
+        temp.append(d[x]['subjectname'])
+        temp.append(d[x]['credit'])
+
+        if "theory" in t['year']['2021'].keys():
+            temp.append(str(t['year']['2021']['theory']['marks']))
+            temp.append(str(t['year']['2021']['theory']['grade']))
+        else:temp.append("-");temp.append("-")
+        
+        p = ast.literal_eval(i[0][2])
+
+        if "practical" in p['year']['2021'].keys():
+            temp.append(str(p['year']['2021']['practical']['marks']))
+            temp.append(str(p['year']['2021']['practical']['grade']))
+        else:temp.append("-");temp.append("-")
+
+        if "theory" in t['year']['2021'].keys() and "practical" in p['year']['2021'].keys():
+            avg=(float(t['year']['2021']['theory']['marks'])+float(p['year']['2021']['practical']['marks']))/2
+        else:
+            avg="-"
+        
+        temp.append(str(avg))
+
+        if avg!="-":
+            for g in grade:
+                if(avg>=g['r1'] and avg<=(float(g['r2'])+0.99)):
+                    temp.append(g['grade'])
+        else:
+             temp.append("-")
+
+        x=x+1
+        print(len(temp))
+        marks.append(temp)
+
+    print(marks)
+
     
 
     return HttpResponse("hii")
