0c657bbcc2701c3bc159e253801971d61bd3d798
Shanu Pandey <shanukumarpandey2003@gmail.com>
2023-09-17 23:44:22 +0530

    commit

--- college_management/COLLEGE_MANAGEMENT/superadmin/urls.py
+++ college_management/COLLEGE_MANAGEMENT/superadmin/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('dashboard/',home),
+    path('dashboard/',dashboard,name="dashboard"),
+    path('branch/',branch,name="branch"),
+    path('branch-add/',branchAdd,name="branch-add"),
+    path('branch-edit/',branchEdit,name="branch-edit"),
+    path('course/',course,name="course"),
+    path('admin/',admins,name="admin"),
+    path('setting/',setting,name="setting"),
 ]
--- college_management/COLLEGE_MANAGEMENT/superadmin/views.py
+++ college_management/COLLEGE_MANAGEMENT/superadmin/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 
-def home(request):
+def dashboard(request):
   
   return render(request, 'dashboard.html',{})
+
+def branch(request):
+  
+  return render(request, 'branch.html',{})
+
+def branchAdd(request):
+  
+  return render(request, 'branch-add.html',{})
+
+def branchEdit(request):
+  
+  return render(request, 'branch-edit.html',{})
+
+def course(request):
+  
+  return render(request, 'course.html',{})
+
+def admins(request):
+  
+  return render(request, 'admin.html',{})
+
+def setting(request):
+  
+  return render(request, 'setting.html',{})
